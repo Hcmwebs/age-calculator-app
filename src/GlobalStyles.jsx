5c186b0f651a15f33e2d025bf26cdcbe563afe26
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-04-27 16:28:33 +1200

    created the GlobalStyles
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  :root{
+
+  }
+
+  body{
+    margin: 0;
+    padding: 0;
+    display:grid;
+    place-items: center;
+    min-height: 100vh;
+  }
+`
+
+
+export default GlobalStyles
