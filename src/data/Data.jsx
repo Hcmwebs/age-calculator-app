d97d17c72fa5c76312ae9a3b0472a148f38171e0
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-04-28 14:09:14 +1200

    updated the data for the inputs
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

---
+++ src/data/Data.jsx
@@ -0,0 +1,7 @@
+const inputs = [
+	{ id: 1, name: 'day', type: 'number', placeholder: 24 },
+	{ id: 2, name: 'month', type: 'number', placeholder: 09 },
+	{ id: 3, name: 'year', type: 'number', placeholder: 1984 },
+]
+
+export {inputs}
