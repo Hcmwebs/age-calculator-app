8c9e652b3cc9ddc3b657963132895e1f2422022d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-04-28 14:28:55 +1200

    changed the placeholder for the month
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -1,7 +1,7 @@
 const inputs = [
 	{ id: 1, name: 'day', type: 'number', placeholder: 24 },
-	{ id: 2, name: 'month', type: 'number', placeholder: 09 },
+	{ id: 2, name: 'month', type: 'number', placeholder:9 },
 	{ id: 3, name: 'year', type: 'number', placeholder: 1984 },
 ]
 
-export {inputs}
+export { inputs }
