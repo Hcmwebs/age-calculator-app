6c8713dad6604e5454bfa59370e147b9ad143f05
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-04-28 13:22:59 +1200

    added color variables
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -2,6 +2,15 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
   :root{
+  --purple: hsl(259, 100%, 65%);
+  --light-red: hsl(0, 100%, 67%);
+
+  --white: hsl(0, 0%, 100%);
+  --off-white: hsl(0, 0%, 94%);
+  --light-grey: hsl(0, 0%, 86%);
+  --smokey-grey: hsl(0, 1%, 44%);
+  --off-black: hsl(0, 0%, 8%);
+
 
   }
 
@@ -19,6 +28,7 @@ html{
     display:grid;
     place-items: center;
     min-height: 100vh;
+    background-color:var(--light-grey)
   }
 
   .container{
