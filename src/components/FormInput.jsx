5e68d7371c1eb7155bddb13cd06b4977fca54228
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-05-01 16:44:09 +1200

    added more attributes to the inputs
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const FormInput = ({ id, placeholder, name, type }) => {
+const FormInput = ({
+	id,
+	placeholder,
+	name,
+	type,
+	minLength,
+	maxLength,
+	birthDate,
+	handleChange,
+}) => {
 	return (
 		<div className='formGroup'>
 			<label htmlFor={name}>{name}</label>
-			<input type={type} placeholder={placeholder} name={name} />
+			<input
+				type={type}
+				placeholder={placeholder}
+				name={name}
+				minLength={minLength}
+				maxLength={maxLength}
+				value={birthDate.value}
+				onChange={handleChange}
+			/>
 		</div>
 	)
 }
