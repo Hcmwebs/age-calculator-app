0e745a5ea6b7ce61f2f4188b1aaa4fc19d4b13bf
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-06-10 23:11:25 +1200

    moved the the values to the appContext
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -3,8 +3,14 @@ import React, { createContext, useContext, useState, useEffect } from 'react'
 const GlobalContext = createContext()
 
 const AppContext = ({ children }) => {
-  const [name, setName] = useState('Heze')
-	return <GlobalContext.Provider value={{name, setName}}>{children}</GlobalContext.Provider>
+	const [birthDate, setBirthDate] = useState({ day: '', month: '', year: '' })
+	const [birthDates, setBirthDates] = useState([])
+	const [error, setError] = useState(false)
+	return (
+		<GlobalContext.Provider value={{ birthDate, birthDates }}>
+			{children}
+		</GlobalContext.Provider>
+	)
 }
 const useGlobalContext = () => useContext(GlobalContext)
 export { AppContext, useGlobalContext }
