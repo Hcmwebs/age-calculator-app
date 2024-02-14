3afe7563d8178d6d73c2fd03b99e89869b6a3048
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-14 13:35:25 +1300

    moved all the functionality to the context

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,39 +1,11 @@
 import React, { useState } from 'react';
 import { inputs } from '../data/Data';
 import { FormInput } from './index';
-import uniqid from 'uniqid';
 import styled from 'styled-components';
+import { useGlobalContext } from '../context/appContext';
 const Form = () => {
-	const [birthDate, setBirthDate] = useState({ day: '', month: '', year: '' });
-	const [birthDates, setBirthDates] = useState([]);
-	const [error, setError] = useState(false);
-	const mnths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-	const today = new Date();
-
-	const currentYr = today.getFullYear();
-	const currentMnth = today.getMonth() + 1;
-	const currentDay = today.getDate();
-
-	const handleChange = (e) => {
-		const value = e.target.value;
-		const name = e.target.name;
-		setBirthDate({ ...birthDate, [name]: value });
-	};
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (!birthDate.day || !birthDate.month || !birthDate.year) {
-			setError(true);
-			console.log('cannot be blank');
-		} else {
-			setError(false);
-			const newId = uniqid();
-			const newBirthDate = { ...birthDate, id: newId };
-			const updatedBirthDates = [...birthDates, newBirthDate];
-			setBirthDates(updatedBirthDates);
-			setBirthDate({ day: '', month: '', year: '' });
-		}
-	};
+	const { handleSubmit, birthDate, birthDates, handleChange } =
+		useGlobalContext();
 	return (
 		<Wrapper>
 			<form onSubmit={handleSubmit}>
@@ -66,70 +38,19 @@ const Form = () => {
 				</div>
 			</form>
 
-			{birthDates.map((birthDate) => {
-				let { day, month, year, id } = birthDate;
-				const diff = (a, b) => a - b;
-				const isLeapYear = (year) => {
-					if (year % 4 === 0 || year % 100 === 0 || year % 400 === 0) {
-						mnths[1] = 29;
-					} else {
-						mnths[1] = 28;
-					}
-				};
-
-				const ageYear = () => {
-					year = diff(currentYr, year);
-				};
-				const ageMonth = () => {
-					if (currentMnth >= month) {
-						month = diff(currentMnth, month - 1);
-					} else {
-						year--;
-						month = 12 + diff(currentMnth, month - 1);
-					}
-				};
-				const ageDay = () => {
-					if (day > mnths[currentMnth - 1]) {
-						day = 0;
-						month++;
-					} else if (currentDay >= day) {
-						day = diff(currentDay, day);
-					} else {
-						month--;
-						let days = mnths[currentMnth - 2];
-						day = days + diff(currentDay, day);
-						if (month === 0) {
-							month = 11;
-							year--;
-						}
-					}
-				};
-				const calculateAge = () => {
-					isLeapYear(currentYr);
-
-					if (
-						year > currentYr ||
-						(month > currentMnth && year === currentYr) ||
-						(day > currentDay && month === currentMnth && year === currentYr)
-					) {
-						alert('Sorry! You are still unborn. Please, be patient');
-						return;
-					}
-
-					ageYear();
-					ageMonth();
-					ageDay();
-				};
-				calculateAge();
-
-				return (
-					<div key={id}>
-						<h2>{year || '--'} years</h2>
-						<h2>{month || '--'} months </h2>
-						<h2>{day || '--'} days</h2>
-					</div>
-				);
-			})}
+			<div className='min-h-10'>
+				<div>
+					<h2>
+						<span>{birthDates.year || '--'}</span> years
+					</h2>
+					<h2>
+						<span> {birthDates.month || '--'}</span>months
+					</h2>
+					<h2>
+						<span>{birthDates.day || '--'}</span>days
+					</h2>
+				</div>
+			</div>
 		</Wrapper>
 	);
 };
--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,16 +1,108 @@
-import React, { createContext, useContext, useState, useEffect } from 'react'
-
-const GlobalContext = createContext()
+import { nanoid } from 'nanoid';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+const GlobalContext = createContext();
 
 const AppContext = ({ children }) => {
-	const [birthDate, setBirthDate] = useState({ day: '', month: '', year: '' })
-	const [birthDates, setBirthDates] = useState([])
-	const [error, setError] = useState(false)
+	const initialState = { day: '', month: '', year: '' };
+	const [birthDate, setBirthDate] = useState(initialState);
+	const [birthDates, setBirthDates] = useState([]);
+	const [error, setError] = useState(false);
+	const mnths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+	const today = new Date();
+
+	const currentYr = today.getFullYear();
+	const currentMnth = today.getMonth() + 1;
+	const currentDay = today.getDate();
+
+	const handleChange = (e) => {
+		const value = e.target.value;
+		const name = e.target.name;
+		setBirthDate({ ...birthDate, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!birthDate.day || !birthDate.month || !birthDate.year) {
+			setError(true);
+			console.log('cannot be blank');
+			return;
+		}
+		setError(false);
+		const newId = nanoid();
+		const newBirthDate = { ...birthDate, id: newId };
+		const updatedBirthDates = [...birthDates, newBirthDate];
+		setBirthDates(updatedBirthDates);
+		console.log(birthDates);
+	};
+
+	const isLeapYear = (year) => {
+		if (year % 4 === 0 || year % 100 === 0 || year % 400 === 0) {
+			mnths[1] = 29;
+		} else {
+			mnths[1] = 28;
+		}
+	};
+
+	const calculateAge = () => {
+		let { id, day, month, year } = birthDates;
+
+		isLeapYear(currentYr);
+
+		if (
+			year > currentYr ||
+			(month > currentYr && year === currentYr) ||
+			(day > currentDay && month === currentMnth && year === currentYr)
+		) {
+		}
+
+		const diff = (a, b) => a - b;
+
+		const ageYear = () => {
+			year = diff(currentYr, year);
+		};
+		const ageMonth = () => {
+			if (currentMnth >= month) {
+				month = diff(currentMnth, month - 1);
+			} else {
+				year--;
+				month = 12 + diff(currentMnth, month - 1);
+			}
+		};
+		const ageDay = () => {
+			if (day > mnths[currentMnth - 1]) {
+				day = 0;
+				month++;
+			} else if (currentDay >= day) {
+				day = diff(currentDay, day);
+			} else {
+				month--;
+				let days = mnths[currentMnth - 2];
+				day = days + diff(currentDay, day);
+				if (month === 0) {
+					month = 11;
+					year--;
+				}
+			}
+		};
+		ageDay();
+		ageMonth();
+		ageYear();
+	};
+
 	return (
-		<GlobalContext.Provider value={{ birthDate, birthDates }}>
+		<GlobalContext.Provider
+			value={{
+				birthDate,
+				birthDates,
+				handleChange,
+				handleSubmit,
+				error,
+				calculateAge,
+			}}
+		>
 			{children}
 		</GlobalContext.Provider>
-	)
-}
-const useGlobalContext = () => useContext(GlobalContext)
-export { AppContext, useGlobalContext }
+	);
+};
+const useGlobalContext = () => useContext(GlobalContext);
+export { AppContext, useGlobalContext };
