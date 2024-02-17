ca069bae2ffd36bb60f56027c2a995ead420c1a3
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-17 23:47:58 +1300

    updated theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,4 +5,21 @@ export default {
 		extend: {},
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: 'hsl(0, 0%, 7%)',
+					secondary: 'hsl(0,0%,100%)',
+					accent: 'hsl(47, 88%, 63%)',
+					neutral: 'hsl(0, 0%, 50%)',
+					'base-100': 'hsl(47, 88%, 63%)',
+					white: 'hsl(0, 0%, 100%)',
+					black: 'hsl(0, 0%, 7%)',
+				},
+			},
+			'fantasy',
+			'halloween',
+		],
+	},
 };
