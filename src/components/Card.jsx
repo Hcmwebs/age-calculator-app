2bb9e4809391419f6ae0b306e84462f1db164f38
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-17 23:39:56 +1300

    moved the divider into the form

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,9 @@
-import { Form, Separator, Age } from './index';
+import { Form, Age } from './index';
 
 const Card = () => {
 	return (
 		<div className='grid grid-col-1 gap-y-8 bg-white w-full max-w-[52.5rem] '>
 			<Form />
-			<Separator />
 			<Age />
 		</div>
 	);
