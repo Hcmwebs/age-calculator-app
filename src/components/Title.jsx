6285806e60d50ebd3a03637a40910e2b728086bf
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-20 09:20:20 +1300

    updated the styles on the title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,38 +1,14 @@
-import React from 'react';
 import styled from 'styled-components';
 
 const Title = () => {
 	return (
-		<div className='grid place-items-center'>
-			<h1 className='text-4xl'>
+		<div className='grid place-items-center gap-y-4 '>
+			<h1 className='text-4xl font-bold italic'>
 				<span className='text-primary'>Age</span> Calculator
 			</h1>
-			<div className='underline'></div>
+			<div className='w-1/3 h-1 bg-primary'></div>
 		</div>
 	);
 };
-const Wrapper = styled.div`
-	width: max-content;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-
-	h1 {
-		font-size: 2rem;
-		font-weight: 800;
-		font-style: italic;
-		text-align: center;
-		margin: 0;
-		span {
-			color: var(--purple);
-		}
-	}
-	.underline {
-		width: 30%;
-		height: 0.25rem;
-		background-color: var(--purple);
-	}
-`;
 
 export default Title;
