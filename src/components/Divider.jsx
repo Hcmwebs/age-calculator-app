e647464ea1fc0d738f6fc70196dff3ae9605f60d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-20 12:42:55 +1300

    styled the divider/ button

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -3,8 +3,12 @@ import btnImg from '../assets/icon-arrow.svg';
 
 const Divider = () => {
 	return (
-		<div className='divider'>
-			<button className='bg-purple-500 rounded-full w-16 h-16'>
+		<div className='relative cursor-pointer'>
+			<div className='divider'></div>
+			<button
+				type='submit'
+				className='bg-purple-500 rounded-full absolute -top-3 lg:-top-6 right-[40%] lg:right-0 w-16 lg:w-24 h-16 lg:h-24 grid place-items-center'
+			>
 				<img src={btnImg} alt='button icon' className='object-cover' />
 			</button>
 		</div>
