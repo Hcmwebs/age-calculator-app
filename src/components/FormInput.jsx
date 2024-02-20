2bc8e9a736f5a550121c4382a7050b246f7d6b72
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-21 09:10:20 +1300

    updated the label state if input is invalid

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -14,27 +14,27 @@ const FormInput = ({
 	const { error } = useGlobalContext();
 	return (
 		<>
-			<div className='form-control gap-y-1 justify-items-center items-center w-full max-w-[100px] lg:max-w-40 group'>
-				<label
-					className='label text-xs uppercase font-bold tracking-[3px] self-start'
-					htmlFor={name}
-				>
-					<span className='text-black group-invalid:text-secondary'>
-						{name}
-					</span>
-				</label>
+			<div className='form-control gap-y-1 justify-items-center items-center w-full max-w-[100px] lg:max-w-40 relative pt-4'>
 				<input
 					type={type}
 					placeholder={placeholder}
 					name={name}
+					id={name}
 					min={min}
 					max={max}
 					value={birthDate.value}
 					onChange={handleChange}
 					className='input input-bordered w-full rounded-lg border-base-100 bg-transparent text-black text-xl tracking-[0.2px] font-bold py-3 px-4 focus:outline-none focus:ring-[1px] focus:ring-primary invalid:ring-secondary
-					invalid:ring-[1px] focus:invalid:ring-secondary focus:invalid:ring-[1px]
-					placeholder:uppercase'
+					invalid:ring-1 focus:invalid:ring-secondary focus:invalid:ring-1
+					placeholder:uppercase
+					peer'
 				/>
+				<label
+					className='label text-xs uppercase font-bold tracking-[3px] self-start absolute -top-3.5 left-0 peer-invalid:text-secondary'
+					htmlFor={name}
+				>
+					{name}
+				</label>
 				{error && (
 					<span className='text-secondary font-normal text-[14px] italic'>
 						{errMsg}
