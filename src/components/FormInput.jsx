760b30f47feac74e39c0f58c3c7fc54b5a8a1fb9
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-20 15:16:49 +1300

    spring cleaned the app

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -32,7 +32,7 @@ const FormInput = ({
 					value={birthDate.value}
 					onChange={handleChange}
 					className='input input-bordered w-full rounded-lg border-base-100 bg-transparent text-black text-xl tracking-[0.2px] font-bold py-3 px-4 focus:outline-none focus:ring-[1px] focus:ring-primary invalid:ring-secondary
-					invalid:border-[1px] invalid:border-secondary focus:invalid:ring-secondary focus:invalid:border-secondary
+					invalid:ring-[1px] focus:invalid:ring-secondary focus:invalid:ring-[1px]
 					placeholder:uppercase'
 				/>
 				{error && (
--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid';
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import getFormValues from '../utils/getFormValues';
 const GlobalContext = createContext();
 
@@ -9,9 +9,6 @@ const AppContext = ({ children }) => {
 	const [birthDate, setBirthDate] = useState(initialState);
 	const [error, setError] = useState(false);
 	const [age, setAge] = useState(initialAge);
-
-	const today = new Date();
-	const currentMnth = today.getMonth();
 
 	const handleChange = (e) => {
 		const value = e.target.value;
@@ -40,10 +37,45 @@ const AppContext = ({ children }) => {
 	const calculateAge = (birthDate) => {
 		const { day, month, year } = birthDate;
 		const newBirthDate = new Date(year, day, month);
-		const newDate = new Date(diff(today, newBirthDate));
-		const years = newDate.getFullYear() - 1970;
-		const months = newDate.getMonth();
-		const days = newDate.getDate() - 1;
+
+		const bDay = newBirthDate.getDate();
+		const bMnth = newBirthDate.getMonth() + 1;
+		const bYr = newBirthDate.getFullYear();
+
+		const today = new Date();
+
+		const currentDay = today.getDate();
+		const currentMnth = today.getMonth() + 1;
+		const currentYr = today.getFullYear();
+
+		const getDaysInMonth = (year, month) => new Date(year, month, 0).getDate();
+
+		let years, months, days;
+
+		years = diff(currentYr, bYr);
+
+		if (currentMnth >= bMnth) {
+			months = diff(currentMnth, bMnth);
+		} else {
+			years--;
+			months = 12 + currentMnth - bMnth;
+		}
+
+		if (currentDay >= bDay) {
+			days = diff(currentDay, bDay);
+		} else {
+			months--;
+			days = getDaysInMonth(bYr, bMnth) + diff(currentDay, bDay);
+		}
+		if (months < 0) {
+			months = 11;
+			years--;
+		}
+
+		if (years < 0) {
+			years = 0;
+		}
+
 		setAge({ years, months, days });
 	};
 
