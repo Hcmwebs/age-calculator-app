28fab26bde7f2733d460594c167f8cd9f39b25a1
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-20 13:23:32 +1300

    removed the GlobalStyles

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { AppContext } from './context/appContext.jsx';
 import './index.css';
-import GlobalStyles from './GlobalStyles.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+	<StrictMode>
 		<AppContext>
 			<App />
 		</AppContext>
-	</React.StrictMode>
+	</StrictMode>
 );
