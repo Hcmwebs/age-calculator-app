18706c9f9dbeadd44b48022f42d8862b72315970
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-22 13:07:46 +1300

    removed the empty values

--- src/utils/getFormValues.jsx
+++ src/utils/getFormValues.jsx
@@ -2,11 +2,10 @@ const getFormValues = (form) => {
 	const formData = new FormData(form);
 
 	const values = [...formData.values()];
-	const isEmpty = values.includes('');
 
 	const data = Object.fromEntries(formData);
 
-	return { isEmpty, data };
+	return { data };
 };
 
 export default getFormValues;
